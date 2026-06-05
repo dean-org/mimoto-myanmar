e80d4d93e120e60336cdec1c095236ef80645a3f
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-06-05 09:43:45 +0530

    Update AuditLogRequestBuilder.java
    Signed-off-by: Dharanidharan-S-04 <dean12345.dd@gmail.com>

--- src/main/java/io/mosip/mimoto/util/AuditLogRequestBuilder.java
+++ src/main/java/io/mosip/mimoto/util/AuditLogRequestBuilder.java
@@ -61,7 +61,7 @@ public class AuditLogRequestBuilder {
         ResponseWrapper<AuditResponseDto> responseWrapper = new ResponseWrapper<>();
         try {
             auditRequestDto.setDescription(description);
-            auditRequestDto.setActionTimeStamp(DateUtils.getUTCCurrentDateTimeString());
+            auditRequestDto.setActionTimeStamp(DateUtils.getRequestTimeString());
             auditRequestDto.setApplicationId(AuditLogConstant.MOSIP_4.toString());
             auditRequestDto.setApplicationName(AuditLogConstant.REGISTRATION_PROCESSOR.toString());
             auditRequestDto.setCreatedBy(AuditLogConstant.SYSTEM.toString());
@@ -112,7 +112,7 @@ public class AuditLogRequestBuilder {
 
             auditRequestDto = new AuditRequestDto();
             auditRequestDto.setDescription(description);
-            auditRequestDto.setActionTimeStamp(DateUtils.getUTCCurrentDateTimeString());
+            auditRequestDto.setActionTimeStamp(DateUtils.getRequestTimeString());
             auditRequestDto.setApplicationId(AuditLogConstant.MOSIP_4.toString());
             auditRequestDto.setApplicationName(AuditLogConstant.REGISTRATION_PROCESSOR.toString());
             auditRequestDto.setCreatedBy(AuditLogConstant.SYSTEM.toString());
